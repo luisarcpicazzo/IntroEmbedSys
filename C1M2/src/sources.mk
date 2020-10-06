000a291182c8095252f07142507453abaa77d7eb
luisarcpicazzo <luis.arc.picazzo@gmail.com>
2020-10-06 02:45:20 -0500

    updated sources.mk and some compile rules in Makefile v1.0

--- C1M2/src/sources.mk
+++ C1M2/src/sources.mk
@@ -8,10 +8,27 @@
 # misuse of this material. 
 #
 #*****************************************************************************
+ 
+COMMON_DIR = ../include/common
+MSP432_DIR = ../include/msp432
+CMSIS_DIR = ../include/CMSIS
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = main.c \
+	  memory.c \
+	  startup_msp432p401r_gcc.c \
+	  system_msp432p401r.c
 
 # Add your include paths to this variable
-INCLUDES = 
 
+COMMON_INCLUDES = $(COMMON_DIR)/memory.h \
+	          $(COMMON_DIR)/platform.h
+
+TARGET_INCLUDES = $(MSP432_DIR)/mspr432p401r.h \
+	          $(MSP432_DIR)/msp_compatibility.h \
+	          $(MSP432_DIR)/system_msp432p401r.h \
+	          $(CMSIS_DIR)/cmsis_gcc.h \
+                  $(CMSIS_DIR)/core_cm4.h \
+                  $(CMSIS_DIR)/core_cmFunc.h \
+                  $(CMSIS_DIR)/core_cmdInstr.h \
+                  $(CMSIS_DIR)/core_cmSimd.h
